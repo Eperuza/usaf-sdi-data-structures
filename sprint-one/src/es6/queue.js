32bea79f5189513b52bb24249b7d939d52dd5932
Eperuza <jeremy.riv@outlook.com>
2021-03-09 15:52:24 -0500

    Done with ES6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,35 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.contents = {}
+    this.numItems = 0
   }
 
+  size () {
+    return this.numItems
+  }
+
+  enqueue (value) {
+    this.numItems++
+    this.contents[this.numItems] = value
+  }
+
+  dequeue () {
+    if (this.numItems > 0){
+      var removedItem = this.contents[1]
+       this.contents[this.numItems - 1] =this.contents[this.numItems]
+      this.numItems--
+      return removedItem
+      // obj['New key'] = obj['old key'];
+
+      /*
+        top end of the line ----
+        5: value
+        4: value
+        3
+        2
+
+        bottom start of the line ---
+      */
+    }
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,24 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.contents = {}
+    this.numItems = 0
   }
+size () {
+  return this.numItems
+}
+
+push (value) {
+  this.numItems++
+  this.contents[this.numItems] = value
+}
+
+pop () {
+  if (this.numItems > 0){
+  var removedItem = this.contents[this.numItems]
+  delete this.contents[this.numItems]
+  this.numItems--
+  return removedItem
+  }
+}
 
 }
