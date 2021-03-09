81d20ad2e489a718f44b00f560fdd6fa0f220cce
Eperuza <79524393+Eperuza@users.noreply.github.com>
2021-03-09 15:56:14 -0500

    Add files via upload

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,8 @@
 var Queue = function() {
-  var someInstance = {};
-
-  // Use an object with numeric keys to store values
-  var storage = {};
-
-  // Implement the methods below
-
-  someInstance.enqueue = function(value) {
-  };
-
-  someInstance.dequeue = function() {
-  };
-
-  someInstance.size = function() {
-  };
-
-  return someInstance;
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
 };
+
+var queueMethods = {};
+
+
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,8 @@
 var Stack = function() {
-  var someInstance = {};
-
-  // Use an object with numeric keys to store values
-  var storage = {};
-
-  // Implement the methods below
-  someInstance.push = function(value) {
-  };
-
-  someInstance.pop = function() {
-  };
-
-  someInstance.size = function() {
-  };
-
-  return someInstance;
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
 };
+
+var stackMethods = {};
+
+
